005bd93873e71f7810738d8847fa0cca3afaa444
AnpangCreator <anpangpawit2@gmail.com>
2023-12-11 18:57:32 +0700

    Add basic runner and quack easter egg

--- src/gplus.c
+++ src/gplus.c
@@ -1,32 +1,95 @@
+
 // headers
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+
 #include <string.h>
+#include <math.h>
+#include <time.h>
 
 // constants
 const char* version = "0.1.0";
 const short year = 2023;
 
+// macros and typedefs
+int i;
+#define repeat(n) for (i = 0; i < n; i++)
+
 typedef unsigned char byte;
 
-// functions
 #define clear() printf("\e[1;1H\e[2J")
 
+// general functions
 void error(char* text) {
     printf("\033[38;5;202m%s\033[0m\n", text);
 }
 
-// run code
-void runCode(char* code) {
+// runner utils
+char code[4096];
+char thisChar;
+int codeLength, charNumber;
 
+void goUntil(char until) {
+    while(code[charNumber] != until) {
+        ++charNumber;
+    }
 }
 
-// main
+// run code
+void runCode() {
+    
+    codeLength = strlen(code);
+    charNumber = 0;
+
+    while(charNumber < codeLength) {
+        thisChar = code[charNumber];
+
+        if(thisChar == ' ') {
+
+            // its a space, do nothing
+
+        } else if(thisChar == '#') {
+
+            // the rest of this line is a comment, so go to next line
+            goUntil('\n');
+
+        } else if(isdigit(thisChar) || thisChar == '.') {
+
+            // its a number literal
+            // coming soon
+
+        } else if(thisChar == '\'') {
+
+            // its a formatted string literal
+            // also coming soon
+
+        } else if(thisChar == '"') {
+
+            // its a string literal
+            // seriously?
+
+        } else {
+            printf("Found unsupported char %c\n", code[charNumber]);
+        }
+
+        ++charNumber;
+    }
+}
+
+// compile code to c
+void compileCode() {
+    error("Compile to C coming soon!");
+    exit(1);
+}
+
+// cli
 int main(int argc, char** argv) {
 
+    srand(time(NULL));
+
     // variables
-    char code[1024] = "";
+    char givenCode[1024] = "";
     char line[256] = "";
     char confirm[1];
 
@@ -38,7 +101,8 @@ int main(int argc, char** argv) {
             "-i   Enter the interpreter console\n"
             "-t   Lets you type code then runs it\n"
             "-r   Run code from a file\n"
-            "-c   Compile code from a file into C code"
+            "-c   Compile code from a file into C code\n"
+            "-q   quack"
         );
         return 1;
 
@@ -55,17 +119,19 @@ int main(int argc, char** argv) {
                 "Press enter twice to run code, type \"exit\" to exit\n"
                 ,version, year
             );
-                
+
             while(true) {
                 printf(">> ");
                 fgets(line, 256, stdin);
                 if(line[1] == '\0') {
-                    runCode(code);
-                    code[0] = '\0';
+                    strcpy(code, givenCode);
+                    runCode();
+                    givenCode[0] = '\0';
                 } else if(strcmp(line, "exit\n") == 0) {
                     error("Exited terminal.");
+                    return 0;
                 } else {
-                    strcat(code, line);
+                    strcat(givenCode, line);
                 }
             }
             
@@ -73,16 +139,17 @@ int main(int argc, char** argv) {
             case 't':
 
             clear();
-            puts("Type your G+ code:");
+            puts("\033[1mType your G+ code:\033[0m");
 
             while(true) {
                 printf("  ");
                 fgets(line, 256, stdin);
                 if(line[1] == '\0') {
-                    runCode(code);
+                    strcpy(code, givenCode);
+                    runCode();
                     return 0;
                 } else {
-                    strcat(code, line);
+                    strcat(givenCode, line);
                 }
             }
 
@@ -97,15 +164,16 @@ int main(int argc, char** argv) {
                 file = fopen(argv[1], "r");
 
                 while(fgets(line, 1024, file)) {
-                    strcat(code, line);
+                    strcat(givenCode, line);
                 }
                 fclose(file);
                 
-                if(code[0] == '\0') {
+                if(givenCode[0] == '\0') {
                     error("File is empty or doesn't exist.");
                     return 1;
                 }
-                runCode(code);
+                strcpy(code, givenCode);
+                runCode();
             } else {
                 puts("Cancelled.");
             }
@@ -117,12 +185,58 @@ int main(int argc, char** argv) {
             fgets(confirm, 1, stdin);
 
             if(confirm[0] == 'y') {
-                puts("Coming soon.");
-                return 1;
+                FILE *file;
+                file = fopen(argv[1], "r");
+
+                while(fgets(line, 1024, file)) {
+                    strcat(givenCode, line);
+                }
+                fclose(file);
+                
+                if(givenCode[0] == '\0') {
+                    error("File is empty or doesn't exist.");
+                    return 1;
+                }
+                strcpy(code, givenCode);
+                compileCode();
             } else {
                 puts("Cancelled.");
             }
         
+            // quack easter egg
+            case 'q':
+
+            double quacks = (double)((double)rand() / (double)(RAND_MAX / 0.35)) + 0.3;
+            int totalQuacks = 0;
+        
+            while(totalQuacks < 1000000) {
+
+                repeat(ceil(quacks)) {
+                    printf("quack ");
+                }
+
+                quacks = quacks * (((double)rand() / (double)(RAND_MAX / 0.7)) + 1.25);
+                totalQuacks = totalQuacks + (int)ceil(quacks);
+                if(quacks > 10000) {
+                    printf("[%d]", totalQuacks);
+                }
+
+                getchar();
+
+            }
+
+            puts("");
+            error(
+                "Congratulations! You've reached over a million quacks in one keypress.\n"
+                "So, please stop now or your computer's gonna turn into the Hiroshima bomb."
+            );
+            return 0;
+
+            // unknown action argument
+            default:
+            error("Unknown action argument.");
+            return 1;
+
         }
     }
 
@@ -135,4 +249,5 @@ Things to know:
   - Numeric types are byte, short, and int (8, 16, and 32 bit)
   - Only allocate enough for what's needed
   - Always initialize strings as empty strings
+  - You can't use = and == with strings! Use strcpy() and strcmp()!
 */
